222f907b38613ea9a9fd8c09a6995b8b9a324554
Isaaclkm <isaacfelixmatos@gmail.com>
2023-03-03 10:54:23 -0500

    added some functionality

--- socket/index.js
+++ socket/index.js
@@ -3,3 +3,27 @@ const io = require('socket.io')(8800, {
         origin: "http://localhost:3000"
     }
 })
+
+let activeUsers = []
+
+io.on('connection', (socket) => {
+    // add new User 
+    socket.on('new-user-add', (newUserId)=>{
+        //if user is not added previously
+        if(!activeUsers.some((user) => user.userId === newUserId))
+        {
+            activeUsers.push({
+                userId: newUserId,
+                socketId: socket.id
+            })
+        }
+        console.log("Connected User", activeUsers)
+        io.emit('get-users', activeUsers)
+    })
+
+    socket.on('disconnect', () => {
+        activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+        console.log('User disconnected', activeUsers)
+        io.emit('get-users', activeUsers)
+    })
+})
